f342ef986fba5e81b487080252ae07ae5594ef99
tout <dudrhkd1319@naver.com>
2023-10-21 15:45:47 +0900

    CUD BUTTON

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 
+// Header component
 function Header(props) {
   return (
     <header>
@@ -12,7 +13,7 @@ function Header(props) {
   )
 }
 
-// props > parameter 값을 통해 내부 설정 
+// Nav component
 function Nav(props) {
   const lis = [
   ]
@@ -36,6 +37,7 @@ function Nav(props) {
   )
 }
 
+// Article Component
 function Article(props) {
   return (
     <article>
@@ -45,20 +47,69 @@ function Article(props) {
   )
 }
 
+// Create Component
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={event => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title,body);
+      }}>
+        <p><input type = "text" name ="title" placeholder='title' /></p>
+        <p><textarea name = "body" placeholder='body'></textarea></p>
+        <p><input type = "submit" value = "create"/></p>
+      </form>
+    </article>
+  )
+}
+
+
+// update Component
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return (
+    <article>
+      <h2>Update</h2>
+      <form onSubmit={event => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onUpdate(title,body);
+      }}>
+        <p><input type = "text" name ="title" placeholder='title' value = {title} onChange={event=>{
+          console.log(event.target.value);
+          setTitle(event.target.value);
+        }}/></p>
+        <p><textarea name = "body" placeholder='body' value = {body} onChange={event=>{
+          setBody(event.target.value);
+        }}></textarea></p>
+        <p><input type = "submit" value = "Update"/></p>
+      </form>
+    </article>
+  )
+}
+
+// main App
 function App() {
   // state 생성 > react에서는 state 값을 통해 내부 컴포넌트의 값을 변경 
-  let [mode, setMode] = useState('WELCOME');
-  let [id, setId] = useState(null);
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
 
   // topics 배열 설정 
-  const topics = [
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is . . .'},
     {id:2, title:'css', body:'css is . . .'},
     {id:3, title:'javascript', body:'javascript is . . .'}
-  ];
+  ]);
 
-  // mode 값을 통해 article 설정 
   let content = null;
+  let contextControl = null;
+  // mode 값을 통해 article 설정 
   if (mode === 'WELCOME') {
     content = <Article title = "Welcome" body = "Hello, Web"></Article>
   } else if (mode === 'READ') {
@@ -69,8 +120,64 @@ function App() {
         body  = topics[idx].body;
       }
     });
-    // {title} 값을 통해 파라미터 값에 맞는 data 설정 bind > ref  
+
     content = <Article title = {title} body = {body}></Article>
+    contextControl = <>
+      <li>
+        <a href={'/update/' + id} onClick={event =>{
+          event.preventDefault();
+          setMode('UPDATE');
+        }}>Update</a>
+      </li>
+      <li>
+        <input type = "button" value = "Delete" onClick ={()=>{
+          const newTopics = [];
+          topics.forEach((val, idx) => {
+            if(val.id !== id) {
+              newTopics.push(topics[idx]);
+            }
+          });
+          setTopics(newTopics);
+          setMode('WELCOME');
+        }} />
+      </li>
+    </>
+    // {title} 값을 통해 파라미터 값에 맞는 data 설정 bind > ref  
+  } else if (mode === 'CREATE') {
+    content = <Create onCreate ={(_title, _body)=> {
+      const newTopic = {id:nextId, title : _title, body : _body};
+      const newTopics = [...topics];  // 배열의 경우 초기화를 해주어야함.
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+
+      // mode 설정변경
+      setMode('READ');
+      setId(nextId);  // id를 next Id로 변경 >> 해당 글 보여주기
+      setNextId(nextId + 1);  // nextId + 1 >> 추후 create
+      
+    }}></Create>
+  } else if(mode === 'UPDATE') {
+    let title, body = null;
+    topics.forEach((val, idx) => {
+      if (val.id === id) {
+        title = topics[idx].title;
+        body  = topics[idx].body;
+      }
+    });
+    content = <Update title={title} body={body} onUpdate={(title, body) => {
+      const newTopics = [...topics];
+      const updateTopic = {id:id, title:title, body:body};
+
+      newTopics.forEach((val, idx) => {
+        if (val.id === id) {
+          newTopics[idx] = updateTopic;
+        }
+      });
+      
+      setTopics(newTopics);
+      setMode('READ');
+    }}>
+    </Update>
   }
   return (
     <div>
@@ -83,6 +190,15 @@ function App() {
         setId(id);
       }}></Nav>
       {content}
+      <ul>
+        <li>
+          <a href ="/create" onClick={event => {
+            event.preventDefault();
+            setMode('CREATE');
+          }}>Create</a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
